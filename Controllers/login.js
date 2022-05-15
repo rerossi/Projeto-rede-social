3d14eb09d4e80913db238491cf6c0e43919f2c58
Rodrigo Rossi <rerossi1985@gmail.com>
2022-05-15 10:42:42 -0300

    ultimas alterações

--- Controllers/login.js
+++ Controllers/login.js
@@ -3,11 +3,11 @@ function logar(){
     let email = document.getElementById('email');
     let senha = document.getElementById('password');
 
-    if(value.email == "mail@mail.com" && value.senha == "admin"){
-
+    if(email.value == "mail@mail.com" && senha.value == "admin"){
+        console.log('acesso')
         localStorage.setItem("acesso", true);
 
-        window.location.href = "index.html";
+        window.location.href = "../index.html";
     }else{
 
         alert('Usuário ou senha inválidos');
