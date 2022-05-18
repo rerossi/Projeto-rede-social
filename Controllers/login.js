b4728ace1d806ad4673e3199c23ddee8b467ee3f
Rodrigo Rossi <rerossi1985@gmail.com>
2022-05-18 15:13:08 -0300

    finalizando o login

---
+++ Controllers/login.js
@@ -0,0 +1,52 @@
+let btnLogin = document.querySelector('#verSenhaLogin');
+
+//evento para visualizar o campo senha
+btnLogin.addEventListener('click', () => {
+    //pega o 
+    let inputPassWord = document.querySelector('#passwordLogin');
+
+
+    if (inputPassWord.getAttribute('type') == 'password') {
+        inputPassWord.setAttribute('type', 'text');
+    } else {
+        inputPassWord.setAttribute('type', 'password');
+
+    }
+});
+
+
+function login() {
+
+    //pega os elementos do formLogin
+    let mail = document.getElementById('mail');
+    let password = document.getElementById('passwordLogin');
+    let msgError = document.querySelector('#msgError')
+
+    let listaUser = [];
+
+    let loginValid = {
+        mail: '',
+        password: ''
+    }
+
+    listaUser = JSON.parse(localStorage.getItem('listaUser'));
+
+    listaUser.forEach((item) => {
+        if (mail.value == item.mailCad && password.value == item.passwordCad) {
+
+            loginValid = {
+                mail: item.mailCad,
+                password: item.passwordCad
+            }
+        }
+    });
+    if (mail.value == loginValid.mail && password.value == loginValid.password) {
+
+        //direciona para a tela principal index
+        const newLocal = window.location.href = "../index.html";
+    } else {
+        msgError.setAttribute('style', 'display: block')
+        msgError.innerHTML = 'Usuário ou senha incorretos'
+    }
+
+}
