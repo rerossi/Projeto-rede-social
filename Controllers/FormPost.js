830096920e64b9072fcceeb3c333b0507fcc6595
Rodrigo Rossi <rerossi1985@gmail.com>
2022-05-27 14:37:48 -0300

    inserindo video

--- Controllers/FormPost.js
+++ Controllers/FormPost.js
@@ -1,55 +1,55 @@
  class FormPost {
 
-    constructor(idForm, idTextarea, idUlPost, idPostImage, idPostVideo) {
+     constructor(idForm, idTextarea, idUlPost, idPostImage, idPostVideo) {
 
-        //pega os elementos do form
-        this.form = document.getElementById(idForm);
-        this.textarea = document.getElementById(idTextarea);
-        this.ulPost = document.getElementById(idUlPost);
-        this.postImage = document.getElementById(idPostImage);
-        this.postVideo = document.getElementById(idPostVideo);
-        this.addSubmit();
-    }
+         //pega os elementos do form
+         this.form = document.getElementById(idForm);
+         this.textarea = document.getElementById(idTextarea);
+         this.ulPost = document.getElementById(idUlPost);
+         this.postImage = document.getElementById(idPostImage);
+         this.postVideo = document.getElementById(idPostVideo);
+         this.addSubmit();
+     }
 
-    onSubmit(func) {
+     onSubmit(func) {
 
-        this.form.addEventListener('submit', func);
+         this.form.addEventListener('submit', func);
 
 
-    }
+     }
 
-    formValidate(value) {
-        //valida os valores do campo texto para publicação
-        if (value === '' || value === null || value === undefined || value.length < 3) {
-            return false
-        }
-        return true
-    }
+     formValidate(value) {
+         //valida os valores do campo texto para publicação
+         if (value === '' || value === null || value === undefined || value.length < 3) {
+             return false
+         }
+         return true
+     }
 
-    //pega a hora atual
-    getTime() {
-        const time = new Date();
-        const hour = time.getHours();
-        const minutes = time.getMinutes();
-        return `${hour}h ${minutes}min`;
-    }
+     //pega a hora atual
+     getTime() {
+         const time = new Date();
+         const hour = time.getHours();
+         const minutes = time.getMinutes();
+         return `${hour}h ${minutes}min`;
+     }
 
-    addSubmit() {
+     addSubmit() {
 
-        const handleSubmit = (event) => {
+         const handleSubmit = (event) => {
 
-            event.preventDefault();
-            if (this.formValidate(this.textarea.value)) {
+             event.preventDefault();
+             if (this.formValidate(this.textarea.value)) {
 
-                const time = this.getTime();
+                 const time = this.getTime();
 
-                //cria um novo post
-                const newPost = document.createElement('li');
-                //adciona um css style
-                newPost.classList.add('post');
+                 //cria um novo post
+                 const newPost = document.createElement('li');
+                 //adciona um css style
+                 newPost.classList.add('post');
 
-                //mostra um elemento Html
-                newPost.innerHTML = `
+                 //mostra um elemento Html
+                 newPost.innerHTML = `
             <div class="infoUserPost">
                 <div class="imgUserPost"></div>
                 <div class="nameAndHour">
@@ -62,14 +62,16 @@
             <p>
             ${this.textarea.value}
             
-            </p>
-           
-            <img src="${this.postImage.src}" style="width:50%;">
-            
-           
-            <video src="${this.postVideo.src}" controls style="width:50%;">
-          
-            <div class="actionBtnPost">
+            </p>`;
+
+                 console.log("Image:" + this.postImage.mostrar)
+                 if (this.postImage.mostrar)
+                     newPost.innerHTML += `<img src="${this.postImage.src}" style="width:30%;">`;
+                 console.log("Video:" + this.postVideo.mostrar)
+                 if (this.postVideo.mostrar)
+                     newPost.innerHTML += `<video src="${this.postVideo.src}" controls style="width:30%;">`;
+
+                 newPost.innerHTML += `<div class="actionBtnPost">
             <button type="button" class="filePost " style="background-color: lightcoral;"><img src="./assets/curtir.png" alt="Curtir"><b class="text-white">Curtir</b></button>
             <button type="button" class="filePost mx-5" style="background-color:lightseagreen"><img src="./assets/comentar.png" alt="Comentar"><b class="text-white">Comentar</b></button>
             <button type="button" class="filePost " style="background-color: deepskyblue;"><img src="./assets/compartilhar.png" alt="compartilhar"><b class="text-white">Compartilhar</b></button>
@@ -77,43 +79,70 @@
             </div>
             `;
 
-                this.ulPost.append(newPost);
-                this.textarea.value = '';
-                this.postImage.src = '';
-                this.postVideo.src = '';
-            } else {
-                alert('Verifique o campo digitado');
-            }
-        }
+                 this.ulPost.append(newPost);
+                 this.textarea.value = '';
+                 this.postImage.src = null;
+                 this.postImage.mostrar = false;
+                 this.postVideo.src = null;
+                 this.postVideo.mostrar = false;
+             } else {
+                 alert('Verifique o campo digitado');
+             }
+         }
 
-        this.onSubmit(handleSubmit)
-    }
+         this.onSubmit(handleSubmit)
+     }
 
-}
+ }
 
-//instancia a classe FormPost
-const postForm = new FormPost('formPost', 'textarea', 'posts', 'uploadImage', 'uploadVideo');
+ //instancia a classe FormPost
+ const postForm = new FormPost('formPost', 'textarea', 'posts', 'uploadImage', 'uploadVideo');
+
+ const flImage = document.querySelector("#flImage");
+ const flVideo1 = document.querySelector("#flVideo");
+ flImage.mostrar = false;
+ flVideo1.mostrar = false;
+
+ flImage.addEventListener("change", function() {
+     const reader = new FileReader();
+     reader.addEventListener("load", () => {
+         const uploaded_image = reader.result;
+         document.querySelector("#uploadImage").mostrar = true;
+         document.querySelector("#uploadImage").src = uploaded_image;
+     });
+     reader.readAsDataURL(this.files[0]);
+ });
+
+ flVideo1.addEventListener("change", function() {
+     const reader = new FileReader();
+     reader.addEventListener("load", () => {
+         const uploaded_video = reader.result;
+         document.querySelector("#uploadVideo").mostrar = true;
+         document.querySelector("#uploadVideo").src = uploaded_video;
+     });
+     reader.readAsDataURL(this.files[0]);
+ });
 
 
-let userLogado = JSON.parse(localStorage.getItem('userLogado'))
+ let userLogado = JSON.parse(localStorage.getItem('userLogado'))
 
-let logado = document.querySelector('#logado')
+ let logado = document.querySelector('#logado')
 
-let postName = document.querySelector('#postName')
+ let postName = document.querySelector('#postName')
 
-logado.innerHTML = `Olá ${userLogado.fullName}`
-postName.innerHTML = `${userLogado.fullName}`
+ logado.innerHTML = `Olá ${userLogado.fullName}`
+ postName.innerHTML = `${userLogado.fullName}`
 
-let photo = document.getElementById('imgPhoto');
-let file = document.getElementById('flImage');
+ let photo = document.getElementById('imgPhoto');
+ let file = document.getElementById('flImage');
 
-photo.addEventListener('click', () => {
-    file.click();
-});
+ photo.addEventListener('click', () => {
+     file.click();
+ });
 
-let myVideo = document.getElementById('myVideo');
-let flVideo = document.getElementById('flVideo');
+ let myVideo = document.getElementById('myVideo');
+ let flVideo = document.getElementById('flVideo');
 
-myVideo.addEventListener('click', () => {
-    flVideo.click();
-});
+ myVideo.addEventListener('click', () => {
+     flVideo.click();
+ });
